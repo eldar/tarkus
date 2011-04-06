c81e1abacd3947fc6f20d373d92b1a1ccfb8ef16
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2011-04-06 23:06:20 +0100

    added core package

---
+++ public/js/core/extension-system.js
@@ -0,0 +1,15 @@
+var tarkus = {};
+
+tarkus.core = {};
+
+tarkus.core.PluginSpec = function()
+{
+    this.
+}
+
+tarkus.core.Plugin = function()
+{
+    this.initialize = function() {};
+    this.extensionsInitialized = function() {};
+    this.aboutToShutdown = function() {};
+}
