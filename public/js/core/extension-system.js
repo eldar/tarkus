e39694f711ec466c8ad79da27e53158422e1f1d4
Eldar Insafutdinov <insafue@mab.(none)>
2011-04-08 11:13:14 +0100

    use requirejs properly

--- public/js/core/extension-system.js
+++ public/js/core/extension-system.js
@@ -1,16 +1,14 @@
-var tarkus = {};
-
-tarkus.core = {};
-
-tarkus.core.PluginSpec = function()
-{
-    this.name = "";
-    this.version = "";
-}
-
-tarkus.core.Plugin = function()
-{
-    this.initialize = function() {};
-    this.extensionsInitialized = function() {};
-    this.aboutToShutdown = function() {};
-}
+define(function() {
+    return {
+        PluginSpec : function() {
+            this.name = "";
+            this.version = "";
+        },
+        
+        Plugin : function() {
+            this.initialize = function() {};
+            this.extensionsInitialized = function() {};
+            this.aboutToShutdown = function() {};
+        }
+    }
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,8 @@
 var deps = [
     'jquery',
     'jquery-ui/jquery-ui-1.8.11.custom.min',
-    'ui-misc/splitter'
+    'ui-misc/splitter',
+    'core/extension-system'
 ];
 
 require(deps);
