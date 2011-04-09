fd1d98c4abe9f46bc0ab6eaea9c1edf8e985ab0d
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2011-04-09 14:53:10 +0100

    moved css out of html

--- public/js/core/edit-area.js
+++ public/js/core/edit-area.js
@@ -4,8 +4,12 @@ var deps = [
 ];
 
 require(deps, function($) {
-    $(".splitarea").splitter({
+    $("#mainsplit").splitter({
         splitVertical: false,
         sizeLeft: true,
     });
+    
+    var newSplit = $("<div id=\"newsplit\" class=\"splitarea\"> <div>first</div> <div>second</div> </div>");
+    $("#rightpane").empty().append(newSplit);
+    $("#newsplit").splitter({splitVertical : false});
 });
