674914ea6e58079a8639d781dbbe520878ec97f0
Eldar Insafutdinov <insafue@mab.(none)>
2011-04-13 11:48:46 +0100

    simplify editor area for now

--- public/js/core/edit-area.js
+++ public/js/core/edit-area.js
@@ -8,9 +8,10 @@ define(deps, function($) {
         splitVertical: false,
         sizeLeft: true,
     });
-    
+/*    
 //  factor this bit of code into separate unit when implementing layout manager
     var newSplit = $("<div id=\"newsplit\" class=\"splitarea\"> <div class=\"splitterpane\"> <div id=\"editor\"></div> </div> <div class=\"splitterpane\">second</div> </div>");
     $("#rightpane").empty().append(newSplit);
     $("#newsplit").splitter({splitVertical : false});
+    */
 });
