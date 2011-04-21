6464fce7e0f96af381a78320b4977819909a028f
Max Samukha <maxter@maxter.com>
2011-04-21 17:13:53 +0300

    naming wankery

--- public/js/core/global.js
+++ public/js/core/global.js
@@ -23,41 +23,41 @@ define(deps, function() {
 
     _.mixin({
     
-        // Classic inheritance
-        inherits: function(superCtor, protoProps, staticProps) {
-            var subCtor;
+        // Classic inheritance (adapted from backbone)
+        inherits: function(superClass, protoProps, staticProps) {
+            var subClass;
 
             // The constructor function for the new class is either defined by you
             // (the "constructor" property in your `extend` definition), or defaulted
             // by us to simply call `super()`.
             if (protoProps && protoProps.hasOwnProperty('constructor')) {
-                subCtor = protoProps.constructor;
+                subClass = protoProps.constructor;
             } else {
-                subCtor = function(){ return superCtor.apply(this, arguments); };
+                subClass = function(){ return superClass.apply(this, arguments); };
             }
 
-            // Set the prototype chain to inherit from `superCtor`, without calling
-            // `superCtor`'s constructor function.
-            ctor.prototype = superCtor.prototype;
-            subCtor.prototype = new ctor();
+            // Set the prototype chain to inherit from `superClass`, without calling
+            // `superClass`'s constructor function.
+            ctor.prototype = superClass.prototype;
+            subClass.prototype = new ctor();
 
-            // Add prototype properties (instance properties) to the subCtor,
+            // Add prototype properties (instance properties) to the subClass,
             // if supplied.
-            if (protoProps) _.extend(subCtor.prototype, protoProps);
+            if (protoProps) _.extend(subClass.prototype, protoProps);
 
             // Add static properties to the constructor function, if supplied.
-            if (staticProps) _.extend(subCtor, staticProps);
+            if (staticProps) _.extend(subClass, staticProps);
 
-            // Correctly set subCtor's `prototype.constructor`, for `instanceof`.
-            subCtor.prototype.constructor = subCtor;
+            // Correctly set subClass's `prototype.constructor`, for `instanceof`.
+            subClass.prototype.constructor = subClass;
             
             // Convenience reference to super constructor
-            subCtor.prototype.super = superCtor;
+            subClass.prototype.super = superClass;
             
-            // Set a convenience property in case the superCtor's prototype is needed later.
-            subCtor.superProto = superCtor.prototype;            
+            // Set a convenience property in case the superClass's prototype is needed later.
+            subClass.superProto = superClass.prototype;            
              
-            return subCtor;
+            return subClass;
         }                
     });
    
