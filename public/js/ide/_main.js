1b9832fc9b59eece32ad2b457ce1bcefe15d5e00
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2011-05-30 16:18:57 +0100

    stub for Open Project dialog

--- public/js/ide/_main.js
+++ public/js/ide/_main.js
@@ -1,26 +1,28 @@
 require({
-  baseUrl: 'js/',
+  baseUrl: "js/",
 
   // set the paths to our library packages
   packages: [
     {
-      name: 'dojo',
-      location: 'dojo-release-1.6.1-src/dojo',
-      main: 'lib/main-browser',
-      lib: '.'
+      name: "dojo",
+      location: "dojo-release-1.6.1-src/dojo",
+      main: "lib/main-browser",
+      lib: "."
     },
     {
-      name: 'dijit',
-      location: 'dojo-release-1.6.1-src/dijit',
-      main: 'lib/main',
-      lib: '.'
+      name: "dijit",
+      location: "dojo-release-1.6.1-src/dijit",
+      main: "lib/main",
+      lib: "."
     }
   ],
 
-  // set the path for the require plugins—text, i18n, etc.
   paths: {
-    text: 'requirejs/text',
-    order: 'requirejs/order',
-    i18n: 'requirejs/i18n'
+    views: "../templates",
+    text: "requirejs/text",
+    order: "requirejs/order",
+    i18n: "requirejs/i18n",
+    ace : "ace/lib/ace",
+    pilot: "pilot/lib/pilot"
   }
 }, ['ide/main']);
