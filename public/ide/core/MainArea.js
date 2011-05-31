944acd7844edcd56f2ddea1e755a6eacb5f61b5e
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2011-05-31 14:58:51 +0400

    Don't display uninitialized markup until scripts are loaded

--- public/ide/core/MainArea.js
+++ public/ide/core/MainArea.js
@@ -2,13 +2,15 @@ var deps = [
    "dojo",
    "dojo/parser",
    "dijit",
+   "text!templates/ide-body.html",
    "dijit/layout/BorderContainer",
    "dijit/layout/ContentPane",
    "dijit/MenuBar",
    "dijit/PopupMenuBarItem"
 ];
 
-define(deps, function (dojo, parser, dijit) {
+define(deps, function (dojo, parser, dijit, bodyView) {
+    dojo.html.set(dojo.body(), bodyView);
     parser.parse();
 
     return {
